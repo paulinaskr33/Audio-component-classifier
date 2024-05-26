eb0390a796a055ade3dab143c8eb32e4583faa59
Karolina Zrobek <karolina.m.zrobek@gmail.com>
2024-05-26 22:12:29 +0200

    CLAP in AudioClassifier class

---
+++ src/audio_classifier.py
@@ -0,0 +1,62 @@
+"""
+Source of this code: https://github.com/microsoft/CLAP/tree/main/examples
+
+This is an example using CLAP for zero-shot inference.
+"""
+from msclap import CLAP
+import torch.nn.functional as F
+
+class AudioClassifier():
+  def __init__(self) -> None:
+    # Load and initialize CLAP
+    # Setting use_cuda = True will load the model on a GPU using CUDA
+    self.clap_model = CLAP(version = '2023', use_cuda=False)
+    
+  def predict(self, classes, audio_files ):
+    # Add prompt
+    prompt = 'this is a sound of '
+    class_prompts = [prompt + x for x in classes]
+
+    # compute text embeddings from natural text
+    text_embeddings = self.clap_model.get_text_embeddings(class_prompts)
+
+    # compute the audio embeddings from an audio file
+    audio_embeddings = self.clap_model.get_audio_embeddings(audio_files, resample=True)
+
+    # compute the similarity between audio_embeddings and text_embeddings
+    similarity = self.clap_model.compute_similarity(audio_embeddings, text_embeddings)
+
+    similarity = F.softmax(similarity, dim=1)
+    values, indices = similarity[0].topk(len(classes))
+
+    return values, indices
+  
+
+if __name__ == '__main__':
+  # Define classes for zero-shot
+  # Should be in lower case and can be more than one word
+  classes = ['coughing','sneezing','drinking sipping', 'breathing', 'brushing teeth']
+  ground_truth = ['coughing']
+  audio_files = ["coughing.wav"]
+
+  clf = AudioClassifier()
+
+  values, indices = clf.classify(classes, audio_files)
+  # Print the results
+  print("Ground Truth: {}".format(ground_truth))
+  print("Top predictions:\n")
+  for value, index in zip(values, indices):
+      print(f"{classes[index]:>16s}: {100 * value.item():.2f}%")
+
+  """
+  The output (the exact numbers may vary):
+
+  Ground Truth: coughing
+  Top predictions:
+
+          coughing: 98.55%
+          sneezing: 1.24%
+  drinking sipping: 0.15%
+        breathing: 0.02%
+    brushing teeth: 0.01%
+  """
